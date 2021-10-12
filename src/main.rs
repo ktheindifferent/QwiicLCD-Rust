97b2923ef2a11d7c701d60e5b45abcc3d9e62ab1
Caleb Smith <calebsmithwoolrich@gmail.com>
2021-10-12 18:45:29 -0400

    ?

--- src/main.rs
+++ src/main.rs
@@ -15,47 +15,49 @@ fn main() {
     // screen.init().unwrap();
 
 
-    println!("clear");
-    screen.clear().unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("clear");
+    // screen.clear().unwrap();
+    // thread::sleep(Duration::from_secs(5));
 
-    println!("enable_cursor");
-    screen.set_cursor(true).unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("enable_cursor");
+    // screen.set_cursor(true).unwrap();
+    // thread::sleep(Duration::from_secs(5));
     
-    println!("disable_cursor");
-    screen.set_cursor(false).unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("disable_cursor");
+    // screen.set_cursor(false).unwrap();
+    // thread::sleep(Duration::from_secs(5));
 
-    println!("enable_cursor");
-    screen.set_cursor(true).unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("enable_cursor");
+    // screen.set_cursor(true).unwrap();
+    // thread::sleep(Duration::from_secs(5));
 
-    println!("enable_blink");
-    screen.set_blink(true).unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("enable_blink");
+    // screen.set_blink(true).unwrap();
+    // thread::sleep(Duration::from_secs(5));
 
-    println!("display_off");
-    screen.set_status(false).unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("display_off");
+    // screen.set_status(false).unwrap();
+    // thread::sleep(Duration::from_secs(5));
 
-    println!("display_on");
-    screen.set_status(true).unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("display_on");
+    // screen.set_status(true).unwrap();
+    // thread::sleep(Duration::from_secs(5));
 
-    println!("home");
-    screen.home().unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("home");
+    // screen.home().unwrap();
+    // thread::sleep(Duration::from_secs(5));
     
-    println!("move_cursor");
-    screen.move_cursor(2,2).unwrap();
-    thread::sleep(Duration::from_secs(5));
+    // println!("move_cursor");
+    // screen.move_cursor(2,2).unwrap();
+    // thread::sleep(Duration::from_secs(5));
     
 
     screen.home().unwrap();
     screen.move_cursor(0,0).unwrap();
     screen.set_blink(false).unwrap();
-    screen.set_cursor(true).unwrap();
+    screen.set_cursor(false).unwrap();
+    screen.clear().unwrap();
+    
     // println!("off");
     // screen.set_backlight(false).unwrap();
     // thread::sleep(Duration::from_secs(5));

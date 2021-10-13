72fd2fc0c0f3f62648f0eb6ded69e676a2646cec
Caleb Smith <calebsmithwoolrich@gmail.com>
2021-10-12 22:19:50 -0400

    fix

--- examples/hello_world/target/debug/deps/hello_world_example-db13e597f61895de.d
+++
@@ -1,5 +0,0 @@
-/home/kal/Documents/PixelCoda/rust-i2c-16x2/examples/hello_world/target/debug/deps/hello_world_example-db13e597f61895de: src/main.rs
-
-/home/kal/Documents/PixelCoda/rust-i2c-16x2/examples/hello_world/target/debug/deps/hello_world_example-db13e597f61895de.d: src/main.rs
-
-src/main.rs:
--- examples/hello_world/target/debug/hello_world_example.d
+++
@@ -1,1 +0,0 @@
-/home/kal/Documents/PixelCoda/rust-i2c-16x2/examples/hello_world/target/debug/hello_world_example: /home/kal/Documents/PixelCoda/rust-i2c-16x2/examples/hello_world/src/main.rs
